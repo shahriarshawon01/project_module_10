72413d8b52759ba069b8ed37a3c164cdf9e46d26
shahriarshawon01 <shahriarshawon001@gmail.com>
2025-02-08 23:19:22 +0600

    hero section done

---
+++ script.js
@@ -0,0 +1,17 @@
+function openTab(evt, tabName) {
+    let i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.classList.add("active");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("starters").style.display = "flex";
+});
